39bfc003ddc11b25a476716f5feb10362312941c
h662 <h662hong@gmail.com>
2023-12-03 15:30:17 +0900

    getToken

--- connect-wallet/src/App.jsx
+++ connect-wallet/src/App.jsx
@@ -1,10 +1,12 @@
 import { useSDK } from "@metamask/sdk-react";
 import { useEffect, useState } from "react";
-import Web3 from "web3";
+import Web3, { Contract } from "web3";
+import MintTokenAbi from "./MintTokenAbi.json";
 
 const App = () => {
   const [account, setAccount] = useState("");
   const [web3, setWeb3] = useState();
+  const [contract, setContract] = useState();
 
   const { sdk, provider } = useSDK();
 
@@ -18,13 +20,37 @@ const App = () => {
     }
   };
 
+  const getToken = async () => {
+    try {
+      const response = await contract.methods.balanceOf(account).call();
+
+      console.log(web3);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     if (!provider) return;
 
     setWeb3(new Web3(provider));
   }, [provider]);
 
-  useEffect(() => console.log(web3), [web3]);
+  useEffect(() => {
+    if (!web3) return;
+    setContract(
+      new web3.eth.Contract(
+        MintTokenAbi,
+        "0xb4A8F0740f9abfd620d69Da915DB37C5a2d56927"
+      )
+    );
+  }, [web3]);
+
+  useEffect(() => {
+    if (!account) return;
+
+    getToken();
+  }, [account]);
 
   return (
     <div className="min-h-screen flex flex-col gap-4 justify-center items-center">
